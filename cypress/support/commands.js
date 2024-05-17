581f90accc4ac67bfb153eaad1d21bbdac70d34a
Talita Rocha <tali_talitarocha@hotmail.com>
2024-05-16 22:29:35 -0300

    Alteração no arquivo commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,6 +12,21 @@ Cypress.Commands.add('token', (email, senha) => {
     })
  })
 
+ Cypress.Commands.add('cadastrarProduto' , (token, produto, preco, descricao, quantidade) =>{
+    cy.request({
+        method: 'POST', 
+        url: 'produtos',
+        headers: {authorization: token}, 
+        body: {
+            "nome": produto,
+            "preco": preco,
+            "descricao": descricao,
+            "quantidade": quantidade
+          }, 
+          failOnStatusCode: false
+    })
+})
+
  Cypress.Commands.add('cadastrarUsuario', (nome, email, password, admin) => {
     cy.request({
         method: 'POST', 
